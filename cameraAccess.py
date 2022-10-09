81b8b17b88333d6d427f7ad5bdad751bc149f21c
Gourav Sutradhar <gouravsutradhar@Gouravs-MacBook-Air.local>
2022-10-10 00:53:00 +0530

    first commit

---
+++ cameraAccess.py
@@ -0,0 +1,26 @@
+import cv2
+
+def Video():
+    cap = cv2.VideoCapture(0)
+
+    width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+    height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+
+    writer = cv2.VideoWriter('Videos/basicvideo.mp4', cv2.VideoWriter_fourcc(*'DIVX'), 20, (width, height))
+
+    while True:
+        ret, frame = cap.read()
+
+        writer.write(frame)
+
+        cv2.imshow('frame', frame)
+
+        if cv2.waitKey(1) & 0xFF == ord('x'):
+            break
+
+    cap.release()
+    writer.release()
+    cv2.destroyAllWindows()
+
+# & 0XFF == ord('x')
+Video()
